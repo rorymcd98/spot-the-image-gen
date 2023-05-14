9a21c2bebf8feefa89e5dcdc157280023bc472a2
Rory McDonald <rorymcd98@gmail.com>
2023-05-14 22:50:17 +0100

    added luncheon of the boating party

--- src/resources/paintingsLibrary.ts
+++ src/resources/paintingsLibrary.ts
@@ -20,6 +20,14 @@ export default {
     "totalDiffs": 11,
     "aspectRatio": 1.45833333333
   },
+  "boating-party": {
+    "name": "Luncheon of the Boating Party",
+    "artist": "Pierre-Auguste Renoir",
+    "year": "1880-1881",
+    "description": "Luncheon of the Boating Party (1880–1881, French: Le déjeuner des canotiers) is a painting by French impressionist Pierre-Auguste Renoir. Included in the Seventh Impressionist Exhibition in 1882, it was identified as the best painting in the show by three critics. It was purchased from the artist by the dealer-patron Paul Durand-Ruel and bought in 1923 (for $125,000) from his son by industrialist Duncan Phillips, who later donated it to the Phillips Collection in Washington, D.C. It shows a richness of form, a fluidity of brush stroke, and a flickering light.",
+    "totalDiffs": 7,
+    "aspectRatio": 1.35458574838,
+  },
   // "test": {
   //   "name": "Test",
   //   "artist": "Test",
